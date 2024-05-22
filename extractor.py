176919daaf7da2da8aea1d1067c5378d02eb4d4e
Jonathan Govinda Medrano Salazar <jonathan.medrano@benandfrank.com>
2024-05-22 17:34:43 -0600

    Create extractor.py

---
+++ extractor.py
@@ -0,0 +1,23 @@
+import yaml
+
+class EnvConfigurator:
+    def __init__(self, yaml_file, env_config_file):
+        self.yaml_file = yaml_file
+        self.env_config_file = env_config_file
+
+    def read_yaml(self):
+        with open(self.yaml_file, 'r') as file:
+            return yaml.safe_load(file)
+
+    def extract_variables(self, data):
+        return data.get('variables', {})
+
+    def save_env_config(self, variables):
+        with open(self.env_config_file, 'w') as file:
+            for key, value in variables.items():
+                file.write(f"{key.upper()}={value}\n")
+
+    def configure_environment(self):
+        data = self.read_yaml()
+        variables = self.extract_variables(data)
+        self.save_env_config(variables)
