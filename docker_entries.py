41a6fc3cbe4880ac987f49e79bc58ec0d6076973
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2024-06-27 14:14:15 -0600

    Create docker_entries.py

---
+++ docker_entries.py
@@ -0,0 +1,14 @@
+entries = [
+    "**/.git",
+    "**/.gitignore",
+    "**/.vscode",
+    "**/coverage",
+    "**/.env",
+    "**/.ssh",
+    "Dockerfile",
+    "README.md",
+    "docker-compose.yml",
+    "**/.DS_Store",
+    "**/venv",
+    "**/env"
+]
