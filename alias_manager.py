41c6b8180da3f084a231072286d6b6359a86b394
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2024-09-03 02:55:42 -0600

    Update alias_manager.py

--- alias_manager.py
+++ alias_manager.py
@@ -160,6 +160,7 @@ alias_manager.add_alias('g_mixed', 'git reset --mixed ')
 alias_manager.add_alias('g_st', 'git status')
 
 alias_manager.add_alias('local_status', 'python git_status_checker.py /home/arithgrey/enid_service/services')
+alias_manager.add_alias('enid_status', 'python git_status_checker.py /home/_enid_service/services')
 
 
 #DOCS
