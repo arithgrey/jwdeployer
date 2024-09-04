7afb35a17c323a042c394402da3306b213e1db11
Jonathan Medrano <jmedranos@findep.com.mx>
2024-09-04 14:13:51 -0600

    [FEAT] devops tools

--- alias/alias_linkerd.py
+++ alias/alias_linkerd.py
@@ -1,6 +1,7 @@
 #linkerd
 def linkerd(alias_manager):
     print("______________LINKERD")
-    alias_manager.add_alias('check_east', 'linkerd check --pre --context east')
-    alias_manager.add_alias('check_west', 'linkerd check --pre --context west')
-    alias_manager.add_alias('clan_multi_clusters', 'cd /home/multicloud/ && ./clean_cluster.sh ')
+    alias_manager.add_alias('l_check_east', 'linkerd check --pre --context east')
+    alias_manager.add_alias('l_check_west', 'linkerd check --pre --context west')
+    alias_manager.add_alias('l_clan_multi_clusters', 'cd /home/multicloud/ && ./clean_cluster.sh ')
+    alias_manager.add_alias('l_check_gateway_multicluster', 'echo "Verificando los gateways..." && linkerd multicluster --context east gateways && linkerd multicluster --context west gateways')
