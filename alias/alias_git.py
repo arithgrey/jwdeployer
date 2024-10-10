625c9a4b0995144c87fda2d087dc61d534083afd
Jonathan Medrano <jmedranos@findep.com.mx>
2024-10-10 12:26:27 -0600

    [FEAT] devops tools

--- alias/alias_git.py
+++ alias/alias_git.py
@@ -2,3 +2,6 @@
 def git(alias_manager):
     print("______________GIT")
     alias_manager.add_alias("g_chore_devops",'git add . && git commit -m "[FEAT] devops tools" && git push')
+    alias_manager.add_alias("g_stash","git stash")
+    alias_manager.add_alias("g_stash_apply","git stash apply")
+    alias_manager.add_alias("g_st","git status")
