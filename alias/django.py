05cf3e3207b0b3b08cde1d4b0c46cf2d4596ba98
Jonathan Medrano <jmedranos@findep.com.mx>
2024-10-30 12:07:19 -0600

    [FEAT] devops tools

--- alias/django.py
+++ alias/django.py
@@ -2,3 +2,4 @@
 def dj(alias_manager):
     print("______________Django")
     alias_manager.add_alias('dj_cl_migrations', 'find . -path "*/migrations/*.py" -not -name "__init__.py" -delete')
+    alias_manager.add_alias('rm_python_cache', 'find . -type d -name "__pycache__" -exec rm -r {} +')
